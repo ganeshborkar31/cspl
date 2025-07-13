c7435a5183834c59e10adc56274c9604c650e942
Ganesh Borkar <ganeshborkar107@gmail.com>
2025-07-13 18:04:52 +0530

    minor changes

--- cuser/settings.py
+++ cuser/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8ayb%*_rj2@q%dk$p8u+ouvyiuygdxapu@odxp_mp)3!35__#k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*',
     'cspl.corniya.com',
